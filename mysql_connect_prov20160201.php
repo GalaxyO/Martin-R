5b37e64f8951743f2d0a53418877d38dc163277f
GalaxyO <martin_renneby@hotmail.com>
2016-02-03 12:56:52 +0100

    PhP Created

---
+++ mysql_connect_prov20160201.php
@@ -0,0 +1,21 @@
+<?php
+
+	DEFINE('DB_USER','root');
+	DEFINE('DB_HOST','localhost');
+	DEFINE('DB_PASSWORD','qwe123');
+	DEFINE('DB_DATABASE','prov20160201');
+		
+		
+	$dbc = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_DATABASE);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+?>
