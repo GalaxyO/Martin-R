3496e4bea2ddc9ed2ed0ab9d51dc1e6924a7bcb8
GalaxyO <martin_renneby@hotmail.com>
2016-04-09 15:30:29 +0200

    Jobbade vidare på login funktionen och dess design

--- www/login.php
+++ www/login.php
@@ -12,12 +12,12 @@ if(@$_SESSION['loggedIn']){ //Kolla om man är inloggad
 <html >
   <head>
     <meta charset="UTF-8">
-    <title>Pure CSS Blurred Video Background Login Box. (fork)</title>
+    <title> HTLML - CSS Webshops Login. (fork)</title>
     
     
     
     
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
 
     
     
@@ -25,6 +25,10 @@ if(@$_SESSION['loggedIn']){ //Kolla om man är inloggad
   </head>
 
   <body>
+
+    <p id= "rubrik"> Login </p>
+    <main>
+      <fomr id="form" action="confirm.php" method="post">
 
     <div class="vid-container">
   <video id="Video1" class="bgvid back" autoplay="false" muted="muted" preload="auto" loop>
@@ -35,10 +39,12 @@ if(@$_SESSION['loggedIn']){ //Kolla om man är inloggad
       <source src="http://shortcodelic1.manuelmasiacsasi.netdna-cdn.com/themes/geode/wp-content/uploads/2014/04/milky-way-river-1280hd.mp4.mp4" type="video/mp4">
     </video>
     <div class="box">
-      <h1>Login</h1>
-      <input type="text" placeholder="Username"/>
-      <input type="text" placeholder="Password"/>
-      <button>Login</button>
+      
+      <label id="user" for="name"></label>
+      <input type="text" name"name" id="name" placeholder="Username" required/>
+      <input type="password" name="password" required/>
+      <td class="center">
+      <input type="submit" id="submit" name="submit" value="Login"/>
       <p>Not a member? <span>Sign Up</span></p>
     </div>
   </div>
@@ -52,35 +58,3 @@ if(@$_SESSION['loggedIn']){ //Kolla om man är inloggad
     
   </body>
 </html>
-	
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8" /> 
-    <title>
-        HTLML - CSS Webshops Login 
-    </title>
-    <link rel="stylesheet" type="text/css" href="css/style.css" />
-</head>
-<body>
-
-	<p id= "rubrik"> Login </p>
-	<main>
-	<form id="form" name="form" action="confirm.php" method="post">
-
-	<div id="block">
-		<label id="user" for="name">p</label>
-		<input type="text" name="name" id="name" placeholder="Username" required/>
-		<label id="pass" for="password">k</label>
-		<input type="password" name="password" id="password" placeholder="Password" required />
-		<td class="center">
-		<input type="submit" id="submit" name="submit" value="Login"/>
-	</div>
-</form>
-<div id="option"> 
-	<p>Login</p> 
-	<a href="#">forgot?</a>
-</div>
-</body>
-</html>
-<!-- SLUT PÅ FORMULÄR -->
